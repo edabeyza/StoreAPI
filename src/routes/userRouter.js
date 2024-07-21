a3f5c959bd3a95dddca471ddb4bf98256b463411
edabeyza <edabeyzaozeren@gmail.com>
2024-07-21 23:36:53 +0300

    user router done

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,4 +1,22 @@
 "use strict"
+
+const { user } = require('../controllers/userController')
+
 /*------------------------------------------------
              STORE API WITH MONGODB
 -------------------------------------------------*/
+
+// For importing Router
+const router = require('express').Router()
+
+router.route('/')
+    .get(user.list)
+    .create(user.create)
+
+router.route('/:userId')
+    .get(user.read)
+    .put(user.update)
+    .patch(user.update)
+    .delete(user.delete)
+
+module.exports = router
