a1392bbea7c10ec4bca925d82429701694ada8f0
edabeyza <edabeyzaozeren@gmail.com>
2024-07-21 22:41:12 +0300

    user model done

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,3 +2,30 @@
 /*------------------------------------------------
              STORE API WITH MONGODB
 -------------------------------------------------*/
+
+const mongoose = require('mongoose')
+
+const UserSchema = mongoose.Schema(
+    {
+        //_id
+
+        email: {
+            type: String,
+            trim: true,
+            unique: true,
+            required: true
+        },
+
+        password: {
+            type: String,
+            trim: true,
+            required:true
+        }
+    },
+    {
+        collection: 'Users',
+        timestamps: true
+    }
+)
+
+module.exports.User = mongoose.model('User', UserSchema)   
